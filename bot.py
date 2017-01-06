2e8f947d41679a819424c1cfce16818afd886034
scriptonist <aravind.techbridge@gmail.com>
2017-01-06 10:08:39 +0530

    completed simple functions and created a skeleton

--- bot.py
+++ bot.py
@@ -5,12 +5,54 @@ from slackclient import SlackClient
 # The bots enivironment variable
 BOT_ID = os.environ.get("BOT_ID")
 
-# Constants
-AT_BOT = "<@"  + BOT_ID + ">"
-EXAMPLE_COMMAND = "do"
-
 #instantiante Slack
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+# Constants
+AT_BOT = "<@"  + BOT_ID + ">"
+
+# Command help strings
+
+COMMAND_HELP = {"create":"Create new remainder format, format - | create <subject> <deadline> |",\
+    "help":"get help for a command, format - | help <command name> |",\
+    "show":"shows currently added remainders format | show |"}
+
+tasklist = {}
+
+def create_remainder(channel,command,command_name):
+    """
+        Creates a remainder
+        @params
+        channel - Channel from which command was posted
+        command - Command that user typed
+        command_name - Command that system identified
+    """
+    global tasklist
+    command = command.split()
+    deadline = ' '.join(command[-3:])
+    task =  ' '.join(command[1:len(command)-3])
+    deadline_obj = time.strptime(deadline,"%b %d %Y")
+    deadline_obj_str = time.strftime('%m/%d/%Y',deadline_obj)
+    tasklist[task] = [deadline_obj,deadline_obj_str]
+    response = "Task Added : " + task + " on " + deadline_obj_str
+
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+
+def get_help(channel,command,command_name):
+    command = command.split()
+    response = COMMAND_HELP[command[1]]
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+def show_tasks(channel,command,command_name):
+    response = ""
+    for task in tasklist:
+        response += task + "\n"
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+                          
+# Functions which handle the different commands
+COMMAND_HANDLERS = {"create":create_remainder,"help":get_help,"show":show_tasks}
 
 
 def handle_command(command, channel):
@@ -19,12 +61,10 @@ def handle_command(command, channel):
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
-    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
-               "* command with numbers, delimited by spaces."
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
-    slack_client.api_call("chat.postMessage", channel=channel,
-                          text=response, as_user=True)
+    for command_list_command in COMMAND_HANDLERS.keys():
+        if command.startswith(command_list_command):
+            COMMAND_HANDLERS[command_list_command](channel,command,command_list_command)
+            
 
 
 def parse_slack_output(slack_rtm_output):
